796d56b7091c4655ff922444254362023f2e3eca
MR. Golu <88421428+govaninevil@users.noreply.github.com>
2022-11-14 05:56:20 -0800

    MAIN

---
+++ script/script.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+
+    
+    $(document).on('click','.menu-btn',function(event){
+        $('body').toggleClass('menu-open');
+    });
+
+    $(document).on('click','.readmore',function(event){
+        $('.collape-body').toggleClass('collape-open');
+    });
+
+    //Horizontal Tab
+    $('#main-from').easyResponsiveTabs({
+        type: 'default', //Types: default, vertical, accordion
+        width: 'auto', //auto or any width like 600px
+        fit: true, // 100% fit in a container
+        tabidentify: 'hor_1', // The tab groups identifier
+        activate: function(event) { // Callback function if tab is switched
+            var $tab = $(this);
+            var $info = $('#nested-tabInfo');
+            var $name = $('span', $info);
+            $name.text($tab.text());
+            $info.show();
+        }
+    });
+    $('#sub-from').easyResponsiveTabs({
+        type: 'default', //Types: default, vertical, accordion
+        width: 'auto', //auto or any width like 600px
+        fit: true, // 100% fit in a container
+        tabidentify: 'hor_2', // The tab groups identifier
+        activate: function(event) { // Callback function if tab is switched
+            var $tab = $(this);
+            var $info = $('#nested-tabInfo');
+            var $name = $('span', $info);
+            $name.text($tab.text());
+            $info.show();
+        }
+    });
+
+    $('#footer-tabbing').easyResponsiveTabs({
+        type: 'default', //Types: default, vertical, accordion
+        width: 'auto', //auto or any width like 600px
+        fit: true, // 100% fit in a container
+        tabidentify: 'hor_3', // The tab groups identifier
+        activate: function(event) { // Callback function if tab is switched
+            var $tab = $(this);
+            var $info = $('#nested-tabInfo');
+            var $name = $('span', $info);
+            $name.text($tab.text());
+            $info.show();
+        }
+    });
+
+
+    $('#details').owlCarousel({
+        loop:true,
+        margin:20,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+    ;
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:5000,
+        slideSpeed: 300, 
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    });
+
+   
+});
